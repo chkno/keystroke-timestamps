a9258ceda1c9a550750a9af8a99130cf6b87c474
Scott Worley <scottworley@scottworley.com>
2013-09-04 11:04:58 -0700

    Add proper error reporting for open failures

--- keystroke-timestamps.c
+++ keystroke-timestamps.c
@@ -2,12 +2,17 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <linux/input.h>
+#include <err.h>
+#include <sysexits.h>
 
 int main ()
 {
   struct input_event i;
   int fd = open("/dev/input/by-path/platform-i8042-serio-0-event-kbd", O_RDONLY);
-  while (fd && read(fd, &i, sizeof(i)) == sizeof(i)) {
+  if (fd < 0) {
+    err(EX_NOINPUT, "Could not open keyboard event file");
+  }
+  while (read(fd, &i, sizeof(i)) == sizeof(i)) {
     if (i.type == 1 && i.value == 1) {
       printf("%ld.%ld\n", i.time.tv_sec, i.time.tv_usec);
       fflush(stdout);
