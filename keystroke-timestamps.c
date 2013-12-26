a722d110d876b97ec87e7616a93f3e990e928bbf
Scott Worley <scottworley@scottworley.com>
2013-12-25 23:17:28 -0800

    Parse options with getopt instead of strcmp

--- keystroke-timestamps.c
+++ keystroke-timestamps.c
@@ -1,14 +1,26 @@
 #include <err.h>
 #include <fcntl.h>
+#include <getopt.h>
 #include <linux/input.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sysexits.h>
 #include <unistd.h>
 
 int main (int argc, char **argv)
 {
-  int print_usec = argc > 1 && strcmp(argv[1], "--usec") == 0;
+  int print_usec = 0;
+  struct option long_options[] = {
+    {"usec",   no_argument,       &print_usec,  1   },
+    {0,        0,                 0,            0   },
+  };
+  while (1) {
+    int c = getopt_long(argc, argv, "", long_options, NULL);
+    if (c == -1) break;
+    if (c != 0) exit(EX_USAGE);
+  }
+
   int fd = open("/dev/input/by-path/platform-i8042-serio-0-event-kbd", O_RDONLY);
   if (fd < 0) {
     err(EX_NOINPUT, "Could not open keyboard event file");
